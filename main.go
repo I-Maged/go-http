f5d5f7c694ed4a24f362343e895e48c1d8fd1d98
I-Maged <magedhanafy@gmail.com>
2023-08-07 17:05:20 +0300

    interfaces

--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		log.Fatal("Error: ", err)
-		// os.Exit(1)
+		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+	// io.Copy(os.Stdout, resp.Body)
+}
+
+func (logWriter) Write(p []byte) (int, error) {
+	fmt.Println(string(p))
+	fmt.Println("Juat wrote this many bytes", len(p))
+	return len(p), nil
 }
